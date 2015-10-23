869d004b912acdc78e4ce94bd03d2e0c2db20f22
Amin sheikhi <aminsheikhi@gmail.com>
2015-10-24 00:25:35 +0330

    removed kendo configuration

--- src/core/start.js
+++ src/core/start.js
@@ -14,24 +14,15 @@ requirejs.config({
         'ui-bootstrap-tpls': '/bower_components/angular-bootstrap/ui-bootstrap-tpls.min',
         'appModule': 'core/app.module',
         'domReady': 'core/services/domReady',
+        'kendo': '/bower_components/js-helper/src/kendo',
+        'JalaliDate': '/bower_components/js-helper/src/kendo/JalaliDate',
 
-        //kendo
-        'kendo/js/core': '/bower_components/js-helper/src/kendo/js/kendo.core',
-        'kendo/js/grid': '/bower_components/js-helper/src/kendo/js/kendo.grid',
-        'kendo/js/datepicker': '/bower_components/js-helper/src/kendo/js/kendo.datepicker',
-        'kendo/js/combobox': '/bower_components/js-helper/src/kendo/js/kendo.combobox',
-        'kendo/js/dropdown': '/bower_components/js-helper/src/kendo/js/kendo.dropdownlist',
-        'kendo/js/messages/fa-IR': '/bower_components/js-helper/src/kendo/js/messages/kendo.messages.fa-IR',
-        'kendo/js/culture/fa-IR': '/bower_components/js-helper/src/kendo/js/messages/cultures/kendo.culture.fa.IR',
-        'JalaliDate': '/bower_components/js-helper/src/kendo/jalaliDate',
-
-        //'plupload': 'lib/external/plupload.full.min',
-        //'inifinitescroll': 'lib/external/jquery.infinitescroll',
         'logger': 'core/services/logger',
         'confirm': 'core/services/confirm',
         'translate': 'core/services/translate',
         'routeResolver': 'core/services/routeResolver',
         'routeConfig': 'app/route.config',
+
 
         //directives
         'menu': 'core/directives/menu',
@@ -108,35 +99,18 @@ requirejs.config({
             exports: 'translate',
             deps: ['appModule']
         },
-        'kendo/js/core': {
-            exports: 'kendo/js/core',
+        'kendo/js/kendo.core': {
+            exports: 'kendo/js/kendo.core',
             deps: ['jQuery', 'JalaliDate']
         },
-        'kendo/js/grid': {
-            exports: 'kendo/js/grid',
-            deps: ['kendo/js/core']
+        'kendo/js/messages/kendo.messages.fa-IR': {
+            exports: 'kendo/js/messages/kendo.messages.fa-IR',
+            deps: ['kendo/js/kendo.grid']
         },
-        'kendo/js/grid': {
-            exports: 'kendo/js/grid',
-            deps: ['kendo/js/core']
+        'kendo/js/cultures/kendo.culture.fa.IR': {
+            exports: 'kendo/js/cultures/kendo.culture.fa.IR',
+            deps: ['kendo/js/kendo.grid']
         },
-        'kendo/js/messages/fa-IR': {
-            exports: 'kendo/js/messages/fa-IR',
-            deps: ['kendo/js/grid']
-        },
-        'kendo/js/culture/fa-IR': {
-            exports: 'kendo/js/culture/fa-IR',
-            deps: ['kendo/js/grid']
-        },
-        'kendo/js/datepicker': {
-            exports: 'kendo/js/datepicker',
-            deps: ['kendo/js/core']
-        },
-        'JalaliDate': {
-            exports: 'JalaliDate'
-        },
-        //'plupload': {exports: 'plupload', deps: ['jQuery']},
-        //'inifinitescroll': {exports: 'inifinitescroll', deps: ['jQuery']}
 
         //directives
         'menu': {
@@ -157,7 +131,10 @@ require([
         'content',
         'logger',
         'confirm',
-        'translate'
+        'translate',
+        'core/directives/numeric',
+        'core/services/gridFilterCellType',
+        'app/config/gridFilterCellTypeConfig'
     ],
     function () {
         angular.module('appModule').init();
