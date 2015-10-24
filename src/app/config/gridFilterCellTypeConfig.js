035af67929a79bddd7beb22dd8d9efa87637f2ba
Amin sheikhi <aminsheikhi@gmail.com>
2015-10-24 13:18:03 +0330

    added  sample for grid filter type cell

--- src/app/config/gridFilterCellTypeConfig.js
+++ src/app/config/gridFilterCellTypeConfig.js
@@ -1,9 +1,21 @@
 define(['appModule'], function () {
     angular.module('appModule').config(function (gridFilterCellTypeProvider) {
 
+        var employee = {
+            showOperators: false,
+            template: function (args) {
+                gridFilterCellTypeProvider
+                    .control
+                    .combo(args.element, {
+                        url: 'api/employees',
+                        display: 'title',
+                        value: 'id'
+                    });
+            }
+        };
+
         gridFilterCellTypeProvider.set({
-            people: "people",
-            product: "product"
+           employee: employee
         });
     })
 })
